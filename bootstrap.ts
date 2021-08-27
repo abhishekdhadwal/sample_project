ca917196a14c5f596ccb02e961cd31f7b41eb9cb
Abhishek Dhadwal <abhishek.stackgeeks@gmail.com>
2021-08-27 21:13:48 +0530

    test cases configured

--- bootstrap.ts
+++ bootstrap.ts
@@ -3,42 +3,73 @@ import * as DAO from './src/DAO/index';
 import * as Models from './src/Models/index';
 import { common_controller } from './src/Controller/index'
 
-const create_admin = async() => {
+const bootstrap_data = async() => {
       try {
 
-        // check admin exist or not
-        let query = { email :"admin@gmail.com" }
+            await create_default_admins()
+            await create_dummy_user()
+
+      }
+      catch(err) {
+            throw err;
+      }
+}
+
+
+const create_default_admins = async() => {
+      try {
+
+            let query = { email :"admin@gmail.com" }
+            let projection = { __v : 0 }
+            let options = { lean : true }
+            let fetch_data : any = await DAO.get_data(Models.Admin, query, projection, options);
+            
+            if(fetch_data.length == 0) {
+    
+                let default_password = 'qwerty';
+                let password = await common_controller.bcrypt_password(default_password)
+
+                let save_data = {
+                  email : "admin@gmail.com",
+                  password : password,
+                  created_at : +new Date()
+                }
+    
+                await DAO.save_data(Models.Admin, save_data);
+    
+            }
+
+      }
+      catch(err) {
+            throw err;
+      }
+}
+
+
+const create_dummy_user = async() => {
+      try {
+
+        // check user exist or not
+        let query = { email :"user@gmail.com" }
         let projection = { __v : 0 }
         let options = { lean : true }
-        let fetch_data : any = await DAO.get_data(Models.Admin, query, projection, options);
+        let fetch_data : any = await DAO.get_data(Models.Users, query, projection, options);
         
         if(fetch_data.length == 0) {
 
             let default_password = 'qwerty';
             let password = await common_controller.bcrypt_password(default_password)
 
-            // create admin  
-            let save_data = [
-                  {
-                        email : "admin@gmail.com",
-                        password : password
-                  },
-                  {
-                        email : "admin_1@gmail.com",
-                        password : password
-                  },
-                  {
-                        email : "admin_2@gmail.com",
-                        password : password
-                  },
-                  {
-                        email : "abd@yopmail.com",
-                        password : password
-                  }
-            ]
+            // create user
+            let save_data = {
+                  name : "dummy_user",
+                  email : "user@gmail.com",
+                  password : password,
+                  created_at : +new Date()
+            }
 
             let options = { multi : true }
-            await DAO.insert_many(Models.Admin, save_data, options);
+            await DAO.save_data(Models.Users, save_data);
 
         }
 
@@ -48,4 +79,6 @@ const create_admin = async() => {
       }
 }
 
-export default create_admin;
+
+
+export default bootstrap_data;
--- src/test/sum.test.ts
+++ src/test/sum.test.ts
@@ -4,9 +4,16 @@ import sum from './sum';
 
 describe("test add function", () => {
   it("should return 15 for add(10,5)", () => {
-    expect(sum(10, 4)).toBe(15);
+    let result = sum(10,5); 
+    expect(result).toBe(14);
+    expect(result).toMatchSnapshot();
   });
-  it("should return 5 for add(2,3)", () => {
-    expect(sum(2, 3)).toBe(5);
+  it("should return 9 for add(3,6)", () => {
+    let response = sum(3,6);
+    expect(response).toBe(9);
+    expect(response).toMatchSnapshot();
   });
-});
+
+})
+
+
